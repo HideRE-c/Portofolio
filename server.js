07002199dcfbd726b4c1245d287c3407dd18fccd
Re <51020029+Re313@users.noreply.github.com>
2020-02-28 15:22:08 +0200

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
-app.listen(3000, () => console.log('port 3000!'));
